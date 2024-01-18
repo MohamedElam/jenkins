1a8c8f2986ed80b0369e2538ec953b5537f47d53
Mohamed El Amri <78266768+MohamedElam@users.noreply.github.com>
2024-01-18 22:32:13 +0100

    Update HelloController.java

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from New Test: test number 106";
+        return "Hello from New Test: test number 110";
     }
 
     @GetMapping("/user")
